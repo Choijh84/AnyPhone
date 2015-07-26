Choijh84/AnyPhone

commit 4c35e1b972481a2c8d739ae12c721fc6bcfa1404

Now supports Japanese mobile numbers, also localized copy, errors, ui

cloud/main.js
@@ -1,25 +1,40 @@
 require("cloud/app.js");
 
-var twilioAccountSid = 'Your-Twilio-Account-Sid';
-var twilioAuthToken = 'Your-Twilio-Auth-Token';
-var twilioPhoneNumber = 'Your-Twilio-Phone-Number';
+var twilioAccountSid = 'ACd7db1db7fab68bd1cef56398e01d0396';
+var twilioAuthToken = 'fa2598c340f8b0f49dd3d941baa6d067';
+var twilioPhoneNumber = '(855)997-2773';
 var secretPasswordToken = 'Something-Random-Here';
+
+var language = "en";
+var languages = ["en", "jp"];
 
 var twilio = require('twilio')(twilioAccountSid, twilioAuthToken);
 
 Parse.Cloud.define("sendCode", function(req, res) {
-	if (!req.params.phoneNumber || req.params.phoneNumber.length != 10) return res.error('Invalid Parameters');
+	var phoneNumber = req.params.phoneNumber;
+	phoneNumber = phoneNumber.replace(/\D/g, '');
+
+	var lang = req.params.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+		language = lang;
+	}
+
+	if (!phoneNumber || (phoneNumber.length != 10 && phoneNumber.length != 11)) return res.error('Invalid Parameters');
 	Parse.Cloud.useMasterKey();
 	var query = new Parse.Query(Parse.User);
-	query.equalTo('username', req.params.phoneNumber + "");
+	query.equalTo('username', phoneNumber + "");
 	query.first().then(function(result) {
 		var min = 1000; var max = 9999;
 		var num = Math.floor(Math.random() * (max - min + 1)) + min;
 
 		if (result) {
+/// REMOVE BEFORE COMMIT
+			result.set("code", num);
+/// REMOVE BEFORE COMMIT
 			result.setPassword(secretPasswordToken + num);
+			result.set("language", language);
 			result.save().then(function() {
-				return sendCodeSms(req.params.phoneNumber, num);
+				return sendCodeSms(phoneNumber, num, language);
 			}).then(function() {
 				res.success();
 			}, function(err) {
@@ -27,11 +42,14 @@ Parse.Cloud.define("sendCode", function(req, res) {
 			});
 		} else {
 			var user = new Parse.User();
-			user.setUsername(req.params.phoneNumber);
+			user.setUsername(phoneNumber);
+/// REMOVE BEFORE COMMIT
+			user.set("code", num);
+/// REMOVE BEFORE COMMIT
 			user.setPassword(secretPasswordToken + num);
+			user.set("language", language);
 			user.setACL({}); 
 			user.save().then(function(a) {
-				return sendCodeSms(req.params.phoneNumber, num);
 			}).then(function() {
 				res.success();
 			}, function(err) {
@@ -45,8 +63,12 @@ Parse.Cloud.define("sendCode", function(req, res) {
 
 Parse.Cloud.define("logIn", function(req, res) {
 	Parse.Cloud.useMasterKey();
-	if (req.params.phoneNumber && req.params.codeEntry) {
-		Parse.User.logIn(req.params.phoneNumber, secretPasswordToken + req.params.codeEntry).then(function (user) {
+
+	var phoneNumber = req.params.phoneNumber;
+	phoneNumber = phoneNumber.replace(/\D/g, '');
+
+	if (phoneNumber && req.params.codeEntry) {
+		Parse.User.logIn(phoneNumber, secretPasswordToken + req.params.codeEntry).then(function (user) {
 			res.success(user._sessionToken);
 		}, function (err) {
 			res.error(err);
@@ -56,11 +78,17 @@ Parse.Cloud.define("logIn", function(req, res) {
 	}
 });
 
-function sendCodeSms(phoneNumber, code) {
+function sendCodeSms(phoneNumber, code, language) {
+	var prefix = "+1";
+	if(typeof language !== undefined && language == "jp") {
+		prefix = "+81";
+	}
+	console.log(prefix);
+
 	var promise = new Parse.Promise();
 	twilio.sendSms({
-		to: '+1' + phoneNumber,
-		from: twilioPhoneNumber,
+		to: prefix + phoneNumber.replace(/\D/g, ''),
+		from: twilioPhoneNumber.replace(/\D/g, ''),
 		body: 'Your login code for AnyPhone is ' + code
 	}, function(err, responseData) {
 		if (err) {

.gitignore
@@ -1,0 +1,1 @@
+config/

cloud/app.js
@@ -1,34 +1,64 @@
 var express = require("express");
 var app = express();
 
+var language = "en";
+var languages = ["en", "jp"];
+
+var strings = require("cloud/strings.js");
+
 app.set('views', 'cloud/views');
 app.set('view engine', 'ejs');
 app.use(express.bodyParser());
 
+function extend(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function (source) {
+        for (var prop in source) {
+            target[prop] = source[prop];
+        }
+    });
+    return target;
+}
+
 app.get('/', function(req, res) {
-  res.render("index.ejs", {})
+  var lang = req.query.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
+
+  res.render("index.ejs", extend(strings[language], {'language': language}));
 });
 
+
 app.post('/', function(req, res) {
+
   var form = req.body;
+
   var phoneNumber = form.phoneNumber;
+
+  var lang = form.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
 
   if(phoneNumber) {
     phoneNumber = phoneNumber.replace(/\D/g, '');
-    if(phoneNumber.length != 10) {
-      res.render("index.ejs", {warning: "You must enter a 10-digit US phone number including area code."});
+    // check that length of number is 10 for US, 10 or 11 for Japan
+    if(language == "en" && phoneNumber.length != 10
+      || language == "jp" && (phoneNumber.length != 10 && phoneNumber.length != 11)) {
+      res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningPhone'], 'language': language}));
     }
-    Parse.Cloud.run("sendCode", {phoneNumber: phoneNumber}).then(function(response){
+    Parse.Cloud.run("sendCode", {phoneNumber: phoneNumber, language: language}).then(function(response){
       if(response){
-        res.render("verify.ejs", {phoneNumber: phoneNumber});
+        res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningTwilio'], 'language': language}));
       } else {
-        res.render("index.ejs", {warning: "Something went wrong. Please check your Twilio configuration."});
+        res.render("verify.ejs", extend(strings[language], {phoneNumber: phoneNumber, 'language': language}));
       }
     }, function(error){
-    res.render("index.ejs", {warning: "Something went wrong. Please try again."});
+      res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningGeneral'], 'language': language}));
     });
   } else { 
-    res.render("index.ejs", {warning: "You forgot to enter your mobile number."})
+    res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningNoNumber'], 'language': language}))
   }
 });
 
@@ -41,10 +71,15 @@ app.post('/verify', function(req, res){
   var code = form.code;
   var phoneNumber = form.phoneNumber;
 
+  var lang = form.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
+
   if(code) {
     code = code.replace(/\D/g, '');
     if(code.length != 4) {
-      res.render("verify.ejs", {warning: "You must enter a 4 digit code texted to your phone number."});
+      res.render("verify.ejs", extend(strings[language], {warning: strings[language]['warningCodeLength'], 'language': language}));
     }
     Parse.Cloud.run("logIn", {codeEntry: code, phoneNumber: phoneNumber}).then(function(response){
       var sessionToken = response;
@@ -55,10 +90,10 @@ app.post('/verify', function(req, res){
       });
 
     }, function(error) {
-      res.render("verify.ejs", {phoneNumber: phoneNumber, warning: "Something went wrong. Please try again."});
+      res.render("verify.ejs", extend(strings[language], {phoneNumber: phoneNumber, warning: strings[language]['warningGeneral'], 'language': language}));
     });
   } else {
-    res.render("verify.ejs", {phoneNumber: phoneNumber, warning: "That verification code is not valid."});
+    res.render("verify.ejs", extend(strings[language], {phoneNumber: phoneNumber, warning:strings[language]['warningCodeInvalid'], 'language': language}));
   }
 });
 
@@ -67,16 +102,20 @@ app.get('/dashboard/:sessionToken', function(req, res) {
 
   Parse.User.become(sessionToken).then(function (user) {
     console.log("logged in as user");
-    res.render("dashboard.ejs", {
+
+    language = user.get("language");
+
+    res.render("dashboard.ejs", extend(strings[language], {
       sessionToken: sessionToken,
       phoneNumber: user.get("username"), 
       name: user.get("name"),
       setting1: user.get("setting1"),
       setting2: user.get("setting2"),
       setting3: user.get("setting3"),
-    });
+      language: user.get("language"),
+    }));
   }, function (error) {
-    console.log("couldn't log in as user");
+    console.log("Could not log in as user.");
     res.redirect("/");
   });
 });
@@ -92,20 +131,27 @@ app.post('/dashboard', function(req, res) {
   var setting2 = form.setting2;
   var setting3 = form.setting3;
 
+  var lang = form.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
+
   Parse.User.become(sessionToken).then(function (user) {
     user.set("name", name);
     user.set("setting1", (setting1 !== undefined && setting1 == "on")?true:false);
     user.set("setting2", (setting2 !== undefined && setting2 == "on")?true:false);
     user.set("setting3", (setting3 !== undefined && setting3 == "on")?true:false);
+    user.set("language", language);
     user.save().then(function(user) {
-      res.render("dashboard.ejs", {
+      res.render("dashboard.ejs", extend(strings[language], {
         sessionToken: sessionToken,
         phoneNumber: user.get("username"), 
         name: user.get("name"),
         setting1: user.get("setting1"),
         setting2: user.get("setting2"),
         setting3: user.get("setting3"),
-      });
+        language: user.get("language"),
+      }));
     }, function(error) {
       console.log(error);
       res.redirect("/");

cloud/strings.js
@@ -1,0 +1,62 @@
+var string = {
+  'en' : {
+    'title': "AnyPhone",
+    'numberDefault': "555-333-6726",
+    'enterPhone': "Please enter your phone number to log in.",
+    'enterPhoneExtra': "This example is limited to 10-digit US number.",
+    'buttonSubmit': "Submit",
+    'buttonVerify': "Verify",
+    'warningPhone': "You must enter a 10-digit US phone number including area code.",
+    'warningTwilio': "Something went wrong. Please check your Twilio configuration.",
+    'warningGeneral': "Something went wrong. Please try again.",
+    'warningNoNumber': "You forgot to enter your mobile number.",
+    'warningCodeLength': "You must enter a 4 digit code texted to your phone number.",
+    'warningCodeInvalid': "That verification code is not valid.",
+    'messageWelcomeDashboard': "Welcome to your Dashboard",
+    'enterCode': "Enter the 4-digit confirmation code",
+    'enterCodeExtra': "It was sent in an SMS message to",
+    'emailLogin': "Your login code for AnyPhone is",
+    'dashboardWelcome': "Welcome to your Dashboard",
+    'dashboardNumber': "Your Number",
+    'dashboardName': "Your Name",
+    'dashboardNameHint': "Nick Fury",
+    'dashboardSetting1': "Orbital Tracking Grid",
+    'dashboardSetting2': "Iron Legion",
+    'dashboardSetting3': "Theta Protocol",
+    'dashboardUpdate': "Update",
+    'dashboardFinished': "Are you finished?",
+    'dashboardLogout': "Logout",
+  },
+  
+  'jp' : {
+    'title': "AnyPhone - エニーフォン",
+    'numberDefault': "3-5159-8200",
+    'enterPhone': "ログインするには電話番号を入力してください。",
+    'enterPhoneExtra': "この例は、米国の10桁と日本の11桁の番号に限定されています。",
+    'buttonSubmit': "送信",
+    'buttonVerify': "確認",
+    'warningPhone': "市外局番を含めた米国の10桁の電話番号、または日本の11桁の電話番号を入力してください。",
+    'warningTwilio': "問題が発生しました。お客様のTwilio設定を確認してください。",
+    'warningGeneral': "問題が発生しました。もう一度試してください。",
+    'warningNoNumber': "携帯番号の入力をお忘れです。",
+    'warningCodeLength': "お客様の電話番号へ送信されたテキストメッセージに記載の4桁のコードを入力してください。",
+    'warningCodeInvalid': "その確認コードは正しくありません。",
+    'messageWelcomeDashboard': "ダッシュボードへようこそ。",
+    'enterCode': "4桁の確認コードを入力してください",
+    'enterCodeExtra': "SMSメッセージにてこちらへ送信されました",
+    'emailLogin': "あなたのエニーフォンのログインコードは",
+    'dashboardWelcome': "ダシボールド　へ　ようこそ",
+    'dashboardNumber': "あなたのばんご",
+    'dashboardName':"あなたのなまえ",
+    'dashboardNameHint': "ニーク　フューリー",
+    'dashboardSetting1': "オービトル　ツラキング グリド",
+    'dashboardSetting2': "アイヨン　リジョン",
+    'dashboardSetting3': "テェタ　プロトコル",
+    'dashboardUpdate': "アップデート",
+    'dashboardFinished': "終わりましたか？",
+    'dashboardLogout': "ログオウト",
+  },
+};
+
+
+module.exports = string;

cloud/views/dashboard.ejs
@@ -11,47 +11,54 @@
     <div class="container">
       <div class="row">
           <div class="col-md-6 col-md-offset-3">
-            <h1>AnyPhone</h1>
-            <h4>Welcome to your Dashboard</h4>
+            <h1><%= title %></h1>
+            <h4><%= dashboardWelcome %></h4>
             <form method="post" action="/dashboard">
               <div class="form-group">
-                <label for="phoneNumber">Your Number</label>
+                <label for="phoneNumber"><%= dashboardNumber %></label>
                 <div class="input-group">
                   <input type="text" class="form-control btn-lg" tabindex="1" value="<%=phoneNumber%>" disabled>
                 </div>
               </div>
               <div class="form-group">
-                <label for="name">Your Name</label>
+                <label for="name"><%= dashboardName %></label>
                 <div class="input-group">
-                  <input type="text" class="form-control btn-lg" tabindex="1" name="name" placeholder="Nick Fury"
+                  <input type="text" class="form-control btn-lg" tabindex="1" name="name" placeholder="<%= dashboardNameHint %>"
                   value="<% if(locals.name !== undefined) { %><%= name%><% } %>">
                 </div>
               </div>
               <div class="form-group">
-                <label for="setting1">Orbital Tracking Grid</label>
+                <label for="setting1"><%= dashboardSetting1 %></label>
                 <div class="input-group">
                   <input type="checkbox" tabindex="1"<% if(locals.setting1 !== undefined && setting1) { %> checked<% } %> name="setting1">
                 </div>
               </div>
               <div class="form-group">
-                <label for="setting2">Iron Legion</label>
+                <label for="setting2"><%= dashboardSetting2 %></label>
                 <div class="input-group">
                   <input type="checkbox" tabindex="2"<% if(locals.setting2 !== undefined && setting2) { %> checked<% } %> name="setting2">
                 </div>
               </div>
               <div class="form-group">
-                <label for="setting3">Theta Protocol</label>
+                <label for="setting3"><%= dashboardSetting3 %></label>
                 <div class="input-group">
                   <input type="checkbox" tabindex="3"<% if(locals.setting3 !== undefined && setting3) { %> checked<% } %> name="setting3">
                 </div>
               </div>
               <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-lg">Update</button>
+                <label for="language">Language</label>
+                <div class="input-group">
+                  <label><input type="radio" tabindex="4"<% if(locals.language !== undefined && language == "en") { %> checked<% } %> name="language" value="en"> English </label><br/>
+                  <label><input type="radio" tabindex="4"<% if(locals.language !== undefined && language == "jp") { %> checked<% } %> name="language" value="jp"> 日本語 </label>
+                </div>
+              </div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-lg"><%= dashboardUpdate %></button>
                 <input type="hidden" name="sessionToken" value="<%=sessionToken%>">
               </div>
             </form>
             <hr>
-            Done for now? <a href="/" class="btn btn-primary">Logout</a>
+            <%=dashboardFinished%> <a href="/" class="btn btn-primary"><%= dashboardLogout %></a>
           </div>
       </div>
     </div>

cloud/views/index.ejs
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>AnyPhone</title>
+    <title><%= title %></title>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href='//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,19 +10,25 @@
   <body>
     <div class="container">
       <div class="row">
+        <div class="col-md-5 col-md-offset-3" style="text-align: right;">
+        <a href="?language=en">English</a> <a href="?language=jp">日本語</a>
+        </div>
+      </div>
+      <div class="row">
           <div class="col-md-6 col-md-offset-3">
-            <h1>AnyPhone</h1>
-            <h4>Please enter your phone number to log in</h4>
-            <h5>This example is limited to 10-digit US numbers.</h5>
+            <h1><%= title %></h1>
+            <h4><%= enterPhone %></h4>
+            <h5><%= enterPhoneExtra %></h5>
             <% if(locals.warning !== undefined) { %><strong><%= warning %></strong><% } %>
             <form method="post">
+              <input type="hidden" name="language" value="<%=language%>">
               <div class="form-group">
                 <div class="input-group">
-                  <input type="text" class="form-control btn-lg" tabindex="1" name="phoneNumber" id="phoneNumber" placeholder="555-333-6726">
+                  <input type="text" class="form-control btn-lg" tabindex="1" name="phoneNumber" id="phoneNumber" placeholder="<%= numberDefault%>">
                 </div>
               </div>
               <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                <button type="submit" class="btn btn-primary btn-lg"><%=buttonSubmit%></button>
               </div>
             </form>
           </div>

cloud/views/verify.ejs
@@ -10,19 +10,25 @@
   <body>
     <div class="container">
       <div class="row">
+        <div class="col-md-5 col-md-offset-3" style="text-align: right;">
+        <a href="?language=en">English</a> <a href="?language=jp">日本語</a>
+        </div>
+      </div>
+      <div class="row">
           <div class="col-md-6 col-md-offset-3">
             <h1>AnyPhone</h1>
-            <h4>Enter the 4-digit confirmation code:</h4>
-            <h5>It was sent in an SMS message to +1<% if(locals.phoneNumber !== undefined) { %><%= phoneNumber%><% } %></h5>
+            <h4><%=enterCode%>:</h4>
+            <h5><%=enterCodeExtra%> +1<% if(locals.phoneNumber !== undefined) { %><%= phoneNumber%><% } %></h5>
             <% if(locals.warning !== undefined) { %><strong><%= warning %></strong><% } %>
             <form method="post" action="/verify">
+              <input type="hidden" name="language" value="<%=language%>">
               <div class="form-group">
                 <div class="input-group">
                   <input type="text" class="form-control btn-lg" tabindex="1" name="code" id="code" placeholder="1234">
                   <input type="hidden" name="phoneNumber" id="phoneNumber" value="<% if(locals.phoneNumber !== undefined) { %><%= phoneNumber%><% } %>">
                 </div>
               </div>
-              <button type="submit" class="btn btn-primary btn-lg">Verify</button>
+              <button type="submit" class="btn btn-primary btn-lg"><%=buttonVerify%></button>
             </form>
 
           </div>

public/css/anyphone.css
@@ -20,3 +20,7 @@
     border-color: #8F13FD;
     box-shadow: none;
 }
+
+a {
+    color: #8F13FD;
+}

commit 09b1a14fae1d42b138cf35e9f0de834b710d27b1

Send code via SMS on user's first access.
This code existed before but was removed in commit 4c35e1b972481a2c8d739ae12c721fc6bcfa1404 which broke the web app on user's first access.

cloud/main.js
@@ -44,6 +44,7 @@ Parse.Cloud.define("sendCode", function(req, res) {
 			user.set("language", language);
 			user.setACL({}); 
 			user.save().then(function(a) {
+				return sendCodeSms(phoneNumber, num, language);
 			}).then(function() {
 				res.success();
 			}, function(err) {